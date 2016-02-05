e08de2d1eb9075e36f34f2e70b0e73995b94b3fb
princesspretzel <brittanga@gmail.com>
2016-02-04 22:00:24 -0500

    add and standardize questions

--- questions.js
+++ questions.js
@@ -1,17 +1,29 @@
 questions = [
   {
-    text: "If you had to construct your perfect significant other from the body parts of the other players, what would you choose?"
+    text: "if you had to construct your perfect significant other from the body parts of the other players, what would you choose?"
   },
   {
-    text: "Assuming there was an outpost set up there, would you take a one-way trip to Mars?"
+    text: "assuming there was an outpost set up there, would you take a one-way trip to Mars?"
   },
   {
-    text: "What are you most proud of that you've done in the last year?"
+    text: "what are you most proud of that you've done in the last year?"
   },
   {
-    text: "Do you believe in reincarnation?"
+    text: "do you believe in reincarnation?"
   },
   {
-    text: "If you had to have NO hair or 4x the amount of hair, body hair, head hair, everything included... which would you choose?"
+    text: "if you had to have NO hair or 4x the amount of hair, body hair, head hair, everything included... which would you choose?"
+  },
+  {
+    text: "what would you do with mind control powers?"
+  },
+  {
+    text: "what do you think human life would be like post-singularity (when humans can download their brains/conscious minds into computers)?"
+  },
+  {
+    text: "do you think people are irreplaceable?"
+  },
+  {
+    text: "do you think some people are better than others?"
   }
 ]
