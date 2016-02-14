daabbc54d262314e82c999cd5ffc0c3aff4a5b05
princesspretzel <brittanga@gmail.com>
2016-02-13 21:46:25 -0500

    fairness! don't let the person who has gone the most get rolled

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-var players;
+var gameState = {};
 
-$('.sample').hide();
 $('.blank-sample').show();
 $('.result').hide();
 $('.before-question').show();
@@ -8,14 +7,15 @@ $('.before-question').show();
 $('.submit').click(function() {
   $('.start').hide();
   $('.before-question').hide();
-  players = $('.players').val();
+  numberPlayers = $('.players').val();
+  gameState = setKeys(numberPlayers);
 });
 
 $('.ask').click(function() {
   $('.sample').hide();
   $('.before-question').hide();
   $('.blank-sample').show();
-  res = Math.floor((Math.random() * players) + 1);
+  res = play();
   $('.result').text(res);
   $('.result').show();
 });
@@ -29,3 +29,41 @@ $('.question').click(function() {
   console.log("q: ", q);
   $('.sample').text(q);
 });
+
+function setKeys(p){
+  for(i = 1; i <= p; i++) {
+    gameState[i] = 0;
+  }
+  return gameState
+}
+
+function dieRoll(players){
+  return Math.floor((Math.random() * players) + 1);
+}
+
+function checkHash(s, players){
+  var check = false;
+  for(i = 1; i <= players; i++) {
+    if (gameState[i] >= gameState[s]) {
+      check = true
+    }
+  }
+  return check
+}
+
+function fairness(players){
+  p = dieRoll(players);
+  if (checkHash(p, players) === true) {
+    return p
+  } else {
+    fairness(players)
+  }
+}
+
+function play(){
+  players = Object.keys(gameState).length
+  p = fairness(players);
+  score = gameState[p] + 1;
+  gameState[p] = score;
+  return p
+}
